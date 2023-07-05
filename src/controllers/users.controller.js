07eb1cab9732e842d92574aa1c1f13549cb28a6d
rohitkr00 <rohitrajput068@gmail.com>
2023-07-05 23:14:31 +0530

    get memeber details of group

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -8,8 +8,8 @@ const addFriends = catchAsync(async (req, res) => {
         userId: req.userId,
         currentDate: req.currentDate
     };
-    const invite_details=await userService.addFriends(mergedBody);
-    res.status(httpStatus.CREATED).send({ message: 'Add friend succesfully', data: {invite_details} });
+    const invite_details = await userService.addFriends(mergedBody);
+    res.status(httpStatus.CREATED).send({ message: 'Add friend succesfully', data: { invite_details } });
 });
 
 const getFriends = catchAsync(async (req, res) => {
@@ -28,8 +28,19 @@ const createGroups = catchAsync(async (req, res) => {
     res.status(httpStatus.CREATED).send({ message: 'Group Create succesfully', data: { group_id: group_id._id } });
 });
 
+
+const getMembersByGroupId = catchAsync(async (req, res) => {
+    const mergedBody = {
+        ...req.body,
+        userId: req.userId,
+        currentDate: req.currentDate
+    };
+    const groupsDetails= await userService.getMembersByGroupId(mergedBody);
+    res.status(httpStatus.OK).send({ message: 'Data Load succesfully', data: { groupsDetails} });
+});
 module.exports = {
     addFriends,
     getFriends,
-    createGroups
+    createGroups,
+    getMembersByGroupId
 };
