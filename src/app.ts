90e4c7fb73fa1be5247c771b9ff8bb2c09c9e632
trailblazingdigiwebs <trailblazingdigiwebs@gmail.com>
2024-07-11 02:28:55 +0530

    minor change

--- src/app.ts
+++ src/app.ts
@@ -66,7 +66,7 @@ app.options("*", cors());
 app.use("/v1", routes);
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, 'src', 'index.html'));
 });
 
 // send back a 404 error for any unknown api request
