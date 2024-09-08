ac1dde14b1dcf9279559286f1004e7d9fd854bea
trailblazingdigiwebs <trailblazingdigiwebs@gmail.com>
2024-09-08 19:55:27 +0530

    fetch user & group settlment api added

--- src/models/settlement.model.js
+++ src/models/settlement.model.js
@@ -1,10 +1,10 @@
 import mongoose from 'mongoose';
 
 const settlementSchema = new mongoose.Schema({
-  paidBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  paidTo: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  paidBy: { type: mongoose.Schema.Types.ObjectId, ref: 'Users', required: true },
+  paidTo: { type: mongoose.Schema.Types.ObjectId, ref: 'Users', required: true },
   amount: { type: Number, required: true },
-  groupId: { type: mongoose.Schema.Types.ObjectId, ref: 'Group' },
+  groupId: { type: mongoose.Schema.Types.ObjectId, ref: 'Groups' },
   creation_date: {
     type: String,
   },
