ae0e99310ad36ee62ee89714968297b44d1e40be
trailblazingdigiwebs <trailblazingdigiwebs@gmail.com>
2024-10-11 14:33:24 +0530

    razorpay live

--- dist/utills/razorpay.js
+++ dist/utills/razorpay.js
@@ -1,5 +1,5 @@
 "use strict";
 var Razorpay = require('razorpay');
-var instance = new Razorpay({ key_id: 'rzp_test_cfXgi2vqOS38cq', key_secret: 'DnCxbfWPShA6eJJfiWwgr1fC' });
+var instance = new Razorpay({ key_id: 'rzp_live_TjoyObMsaNibqV', key_secret: 'YmjLEKvIIKK33x9O25V4IkUK' });
 module.exports = instance;
 //# sourceMappingURL=razorpay.js.map
--- src/utills/razorpay.js
+++ src/utills/razorpay.js
@@ -1,5 +1,5 @@
 const Razorpay = require('razorpay');
 
-const instance = new Razorpay({ key_id: 'rzp_test_cfXgi2vqOS38cq', key_secret: 'DnCxbfWPShA6eJJfiWwgr1fC' })
+const instance = new Razorpay({ key_id: 'rzp_live_TjoyObMsaNibqV', key_secret: 'YmjLEKvIIKK33x9O25V4IkUK' })
 
 module.exports = instance;
