375492f3502559c5a861fab9dfbdf46468b70d62
trailblazingdigiwebs <trailblazingdigiwebs@gmail.com>
2024-10-12 00:45:24 +0530

    razorpay live

--- src/utills/razorpay.js
+++ src/utills/razorpay.js
@@ -1,5 +1,6 @@
 const Razorpay = require('razorpay');
 
-const instance = new Razorpay({ key_id: 'rzp_live_TjoyObMsaNibqV', key_secret: 'YmjLEKvIIKK33x9O25V4IkUK' })
+// const instance = new Razorpay({ key_id: 'rzp_live_TjoyObMsaNibqV', key_secret: 'YmjLEKvIIKK33x9O25V4IkUK' })
+const instance = new Razorpay({ key_id: 'rzp_test_cfXgi2vqOS38cq', key_secret: 'DnCxbfWPShA6eJJfiWwgr1fC' })
 
 module.exports = instance;
